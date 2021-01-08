1db649372888d47f144604192519172bb4de9b6b
Eduardo Diaz <lalo.diaz1896@gmail.com>
2021-01-08 00:13:41 -0600

    TodosContext

--- context/TodosContext.js
+++ context/TodosContext.js
@@ -3,7 +3,88 @@ import { useState, createContext } from "react";
 const TodosContext = createContext();
 
 const TodosProvider = ({ children }) => {
+  const [todos, setTodos] = useState([]);
+
+  const refreshTodos = async () => {
+    try {
+      const response = await fetch("/api/getTodos");
+      const latestTodos = await response.json();
+      setTodos(latestTodos);
+    } catch (error) {
+      console.error(error)
+    }
+  }
+
+  const addTodo = async (description) => {
+    try {
+      const response = await fetch("/api/createTodo", {
+        method: 'POST',
+        body: JSON.stringify({ description }),
+        headers: {
+          "Content-Type": "application/json"
+        }
+      });
+      const newTodo = await response.json();
+      setTodos((prevTodos) => {
+        [newTodo, ...prevTodos]
+      })
+    } catch (error) {
+      console.error(error)
+    }
+  }
+
+  const updateTodo = async (updatedTodo) => {
+    try {
+      const response = await fetch("/api/updateTodo", {
+        method: 'PUT',
+        body: JSON.stringify(updatedTodo),
+        headers: {
+          "Content-Type": "application/json"
+        }
+      });
+      await response.json();
+      setTodos((prevTodos) => {
+        const existingTodos = [...prevTodos];
+        const existingTodo = existingTodos.find(todo => todo.id === updatedTodo.id);
+        existingTodo.fields = updatedTodo.fields;
+        return existingTodos;
+      })
+    } catch (error) {
+      console.error(error)
+    }
+  }
+
+  const deleteTodo = async ({ id }) => {
+    try {
+      const response = await fetch("/api/deleteTodo", {
+        method: 'DELETE',
+        body: JSON.stringify({ id }),
+        headers: {
+          "Content-Type": "application/json"
+        }
+      });
+      setTodos((prevTodos) => {
+        return prevTodos.filter((todo) => {
+          todo.id !== id
+        })
+      })
+    } catch (error) {
+      console.error(error)
+    }
+  }
+
   return (
-    <TodosContext.Provider>{children}</TodosContext.Provider>
+    <TodosContext.Provider value={{
+      todos,
+      setTodos,
+      refreshTodos,
+      updateTodo,
+      deleteTodo,
+      addTodo,
+    }}>
+      {children}
+    </TodosContext.Provider>
   )
-}
+};
+
+export { TodosProvider, TodosContext };
